6722f18635e90b01bd8f817a6362e075c542d74b
Damyres <damyresmaciell@gmail.com>
2024-08-19 19:32:08 -0300

    Criar evento para inserir item no input de text

--- main.js
+++ main.js
@@ -1,30 +1,9 @@
-// Represetação de lista com array
-// let listaDeCompras = ['Arroz', 'feijao', 'café']
+let listaDeItens = []
 
-// console.table(listaDeCompras)
+const form = document.getElementById("form-itens")
+const itensInput = document.querySelector("receber-item")
 
-// Represetação de lista com objeto
-// const listaDeItens = {
-//     item1: 'biscoito',
-//     item2: 'pão',
-//     quantidade1: 1,
-//     quantidade2: 2,
-//     mostrarItens: function(){
-//         alert('Comprei ' + listaDeItens.quantidade1 + 'pacotes de ' + listaDeItens.item1)
-//     }
-// }
-// listaDeItens.mostrarItens()
-
-//Exemplo de criação de objeto e adicão de propriedades/atributos ao objeto
-const cliente = {nome: 'Mariana'}
-cliente.idade = 76
-cliente.peso = 1.76
-cliente.endereco = 'Avenida A, número 6'
-
-
-//criando um método
-cliente['estado'] = 'Pernambuco'
-
-alert('Olá, meu nome é ' + cliente.nome + 'e moro no estado de ' + cliente.estado + '.')
-console.log(cliente)
+form.addEventListener("submit", function(evento){
+    evento.preventDefault()
+})
 
