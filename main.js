005a354fcde2ce50a4a4a191eaee554cfc5df00a
Damyres <damyresmaciell@gmail.com>
2024-08-19 20:20:58 -0300

    Criar método para inserir mais intes npo mesmo array impossibilitando a substituição do item

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
 
 form.addEventListener("submit", function (evento) {
-    event.preventDefault()
+    evento.preventDefault()
     salvarItem()
 })
 
 function salvarItem() {
     const comprasItem = itensInput.value
 
-    listaDeItens = {
+    listaDeItens.push ({
         valor: comprasItem
-    }
+    })
     console.log(listaDeItens)
 }
