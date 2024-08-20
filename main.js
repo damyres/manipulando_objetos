0c2b923581fa01519f83df14380f95a6379a9b8a
Damyres <damyresmaciell@gmail.com>
2024-08-20 19:20:25 -0300

    Criar função para itens já comprado serem inseridos em uma nova lista de comprados

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ let listaDeItens = []
 const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
 const ulItens = document.getElementById("lista-de-itens")
+const ulItensComprados = document.getElementById("itens-comprados")
 
 form.addEventListener("submit", function (evento) {
     evento.preventDefault()
     salvarItem()
+    mostrarItens()
 })
 
 function salvarItem() {
@@ -21,15 +23,30 @@ function salvarItem() {
             checar: false
         })
     }
-    mostrarItens()
 
     console.log(listaDeItens)
 }
 function mostrarItens() {
     ulItens.innerHTML = ''
+    ulItensComprados.innerHTML = ''
     listaDeItens.forEach((elemento, index) => {
-        ulItens.innerHTML += 
-        `<li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+
+        if (elemento.checar) {
+            ulItensComprados.innerHTML += `
+             <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                <div>
+                    <input type="checkbox" checked class="is-clickable" />  
+                    <span class="itens-comprados is-size-5">${elemento.valor}</span>
+                    </div>
+                <div>
+                    <i class="fa-solid fa-trash is-clickable deletar"></i>
+                </div>
+            </li>
+            `
+        } else {
+
+            ulItens.innerHTML +=
+                `<li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
             <div>
                 <input type="checkbox" class="is-clickable" />
                 <input type="text" class="is-size-5" value="${elemento.valor}"></input>
@@ -38,14 +55,15 @@ function mostrarItens() {
                     <i class="fa-solid fa-trash is-clickable deletar"></i>
                 </div>
         </li>`
+        }
     })
     const inputCheck = document.querySelectorAll('input[type="checkbox"]')
 
     inputCheck.forEach(i => {
         i.addEventListener('click', (evento) => {
-          const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
-          listaDeItens[valorDoElemento].checar = evento.target.checked
-          console.log(listaDeItens[valorDoElemento].checar)
+            const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            listaDeItens[valorDoElemento].checar = evento.target.checked
+            mostrarItens()
 
         })
 
