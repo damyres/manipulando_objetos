729d7e1e1c55c14f37d74d051cecf6544afd2382
Damyres <damyresmaciell@gmail.com>
2024-08-21 19:47:27 -0300

    Ajustar erros no código

--- main.js
+++ main.js
@@ -43,22 +43,21 @@ function salvarItem() {
 function mostrarItens() {
     ulItens.innerHTML = ''
     ulItensComprados.innerHTML = ''
-    listaDeItens.forEach((elemento, index) => {
 
+    listaDeItens.forEach((elemento, index) => {
         if (elemento.checar) {
             ulItensComprados.innerHTML += `
-             <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+            <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
                 <div>
                     <input type="checkbox" checked class="is-clickable" />  
                     <span class="itens-comprados is-size-5">${elemento.valor}</span>
-                    </div>
-                <div>
-                    <i class="fa-solid fa-trash is-clickable deletar"></i>
                 </div>
+                    <div>
+                        <i class="fa-solid fa-trash is-clickable deletar"></i>
+                    </div>
             </li>
             `
         } else {
-
             ulItens.innerHTML +=
                 `<li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
             <div>
@@ -66,17 +65,18 @@ function mostrarItens() {
                 <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number(itemEditar) ? 'disabled' : ''}></input>
             </div>
                 <div>
-                   ${index === Number(itemEditar) ? '<button onclick"salvarEdicao()"> <i class="fa-regular fa-floppy-disk is-clickable"></i> </button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
-        <i class="fa-solid fa-trash is-clickable deletar"></i>
+                    ${index === Number(itemEditar) ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i> '}
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
                 </div >
         </li > `
         }
     })
+
     const inputCheck = document.querySelectorAll('input[type="checkbox"]')
 
     inputCheck.forEach(i => {
         i.addEventListener('click', (evento) => {
-            const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
             listaDeItens[valorDoElemento].checar = evento.target.checked
             mostrarItens()
         })
@@ -91,6 +91,7 @@ function mostrarItens() {
         })
     })
     const editarItens = document.querySelectorAll(".editar")
+
     editarItens.forEach(i => {
         i.addEventListener('click', (evento) => {
             itemEditar = evento.target.parentElement.parentElement.getAttribute('data-value')
@@ -101,6 +102,10 @@ function mostrarItens() {
 }
 
 function salvarEdicao() {
-    const itemEditado = document.querySelector(`[data - value"${itemEditar}] input[type=text"]`)
-    console.log(itemEditado.value)
+    const itemEditado = document.querySelector(`[data-value="${itemEditar}"] input[type="text"]`)
+    //console.log(itemEditado.value)
+    listaDeItens[itemEditar].valor = itemEditado.value
+    console.log(listaDeItens)
+    itemEditar = -1
+    mostrarItens()
 }
